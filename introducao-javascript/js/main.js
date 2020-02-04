b4d2a54011d50ac98a7aaf57ed908abea74573be
mariagiordane <58854064+mariagiordane@users.noreply.github.com>
2020-02-04 16:54:59 -0300

    first commit

---
+++ introducao-javascript/js/main.js
@@ -0,0 +1,33 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Virginia Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var calcImc = paciente.querySelector(".info-imc")
+
+var pesoValido = true;
+var alturaValida = true;
+
+if(peso <= 0 || peso >= 250){
+    console.log("Peso inválido.");
+    pesoValido = false;
+    calcImc.textContent = "Peso Inválido"
+}
+
+if(altura <= 0 || altura >= 3){
+    console.log("Altura inválida.");
+    alturaValida = false;
+    calcImc.textContent = "Altura Inválida"
+}
+
+if(alturaValida && pesoValido) {
+    var imc = peso / (altura * altura);
+    calcImc.textContent = imc
+}
+
