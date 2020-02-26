0185c23d93de9ba00a3db3218b3863da5d8d9ff3
mariagiordane <58854064+mariagiordane@users.noreply.github.com>
2020-02-26 18:31:27 -0300

    appendChild; EventListener; preventDefault
    appendChild; addEventListener; event.preventDefault

--- introducao-javascript/js/main.js
+++ introducao-javascript/js/main.js
@@ -20,14 +20,18 @@ for( var i = 0; i < pacientes.length ; i++){
     
     if(peso <= 0 || peso >= 250){
         console.log("Peso inválido.");
-        pesoValido = false;
         calcImc.textContent = "Peso Inválido"
+        pesoValido = false;
+
+        paciente.classList.add("paciente-invalido");
     }
     
     if(altura <= 0 || altura >= 3){
         console.log("Altura inválida.");
-        alturaValida = false;
         calcImc.textContent = "Altura Inválida"
+        alturaValida = false;
+
+        paciente.classList.add("paciente-invalido");
     }
     
     if(alturaValida && pesoValido) {
@@ -36,5 +40,52 @@ for( var i = 0; i < pacientes.length ; i++){
     }
     
 }
+//escutador de evento
+titulo.addEventListener("click", function(){
+    console.log("Olá eu fui clicado!");
+});
+/* OU PODEMOS USAR A FUNÇÃO NOMEADA ABAIXO, sempre sem parênteses.
+Pois se usarmos a função abaixo com parênteses para o evento click, será  
+associado o retorno da função, que será 0.*/
+function mostraMensagem(){
+    console.log("Olá eu fui clicado!");
+}
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
+//transformar o valor de cada input em uma variável
+    var form = document.querySelector("#form-adiciona");
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+//transformar as variáveis em itens da tabela
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+//acrescentar a tabela ao html
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+
+});
 
